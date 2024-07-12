96b06b7ef1e62b2fa11610783fe7ef95e65c7514
mimiMonads <mimi@monads.dev>
2024-07-12 21:13:03 +0100

    allow origin

--- src/paths/root.ts
+++ src/paths/root.ts
@@ -4,7 +4,9 @@ import { globalOptions } from "../globalOptions.ts";
 export default wrap(globalOptions)()
   .customPetition({
     path: "/ping",
-    f: ({ headers }) => new Response('pong', {
-      headers
+    f: () => new Response('pong', {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
     }),
   });
