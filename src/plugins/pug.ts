086e0a020b2f8909144f9d1b42cfda436832d8bf
mimiMonads <mimi@monads.dev>
2024-07-13 18:48:16 +0100

    adding cors to the main tempalte

--- src/plugins/pug.ts
+++ src/plugins/pug.ts
@@ -1,11 +1,14 @@
 import { pugStaticServerPlugin } from "vixeny-perspective";
+import { globalOptions } from "../globalOptions.ts";
 import { composer } from "vixeny";
 import * as pugModule from "pug";
 
 export default pugStaticServerPlugin(pugModule.compileFile)({
   preserveExtension: false,
   default: { name: 'avant' },
-  petition: composer.objectNullRequest()({
+  petition: composer.objectNullRequest(
+    globalOptions
+  )({
     f: ({ query }) => 
       query?.name 
       ? query 
