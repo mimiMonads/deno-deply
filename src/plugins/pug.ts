25494f2a2493dcad585dd3a79c81702174e4edfc
mimiMonads <mimi@monads.dev>
2024-07-13 18:28:50 +0100

    adding hello for supporting the main website

--- src/plugins/pug.ts
+++ src/plugins/pug.ts
@@ -1,6 +1,14 @@
 import { pugStaticServerPlugin } from "vixeny-perspective";
+import { composer } from "vixeny";
 import * as pugModule from "pug";
 
 export default pugStaticServerPlugin(pugModule.compileFile)({
   preserveExtension: false,
+  default: { name: 'avant' },
+  petition: composer.objectNullRequest()({
+    f: ({ query }) => 
+      query?.name 
+      ? query 
+      : null
+  })
 });
